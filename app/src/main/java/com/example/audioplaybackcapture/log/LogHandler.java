1afb2def5f53b3b8e7868949c71c1e74bf378af7
RajibCP <rajib.paul1108025@gmail.com>
2020-02-05 20:35:33 +0600

    Create Log class.

---
+++ app/src/main/java/com/example/audioplaybackcapture/log/LogHandler.java
@@ -0,0 +1,9 @@
+package com.example.audioplaybackcapture.log;
+
+import android.util.Log;
+
+public class LogHandler {
+    public static void d(String tag, String message){
+        Log.d(tag, message);
+    }
+}
