66b80fcb54878b1e7deb97bbf277e644c1bd011c
RajibCP <rajib.paul1108025@gmail.com>
2020-02-22 01:13:18 +0600

    Added a foreground service.

---
+++ app/src/main/java/com/example/audioplaybackcapture/service/AudioForegroundService.java
@@ -0,0 +1,69 @@
+package com.example.audioplaybackcapture.service;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+
+import androidx.core.app.NotificationCompat;
+
+import com.example.audioplaybackcapture.MainActivity;
+import com.example.audioplaybackcapture.log.LogHandler;
+
+public class AudioForegroundService extends Service {
+
+    public final String CHANNEL_ID =  "AudioForegroundServiceChannel";
+    private final String TAG = AudioForegroundService.class.getSimpleName();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        LogHandler.d(TAG, "onStartCommand in Audio Service");
+        createNotificationChannel();
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0, notificationIntent, 0);
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Audio Foreground Service")
+                .setContentText("Audio Forground Service Example in Android")
+                .setContentIntent(pendingIntent)
+                .build();
+
+        startForeground(1, notification);
+
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Audio Foreground Service Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+}
